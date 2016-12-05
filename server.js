9a62f9ca1212d6c1f65c33c53f322991d1ca8a87
Arthur Lockman <hello@rthr.me>
2016-12-05 18:36:31 -0500

    Added firebase connection

--- server.js
+++ server.js
@@ -1,57 +1,23 @@
 var http = require('http')
   , fs   = require('fs')
-  , marked = require('marked')
   , url  = require('url')
-  , querystring = require('querystring')
-  , port = 8080
+  , admin = require("firebase-admin")
 
-var server = http.createServer (function (req, res) {
-  var uri = url.parse(req.url)
+//Configure firebase connection
+var serviceAccount = require("./firebase.json")
 
-  switch( uri.pathname ) {
-    case '/':
-    case '/index.html':
-        sendFile(res, 'index.html', 'text/html')
-        break
-    case '/css/style.css':
-      sendFile(res, 'style.css', 'text/css')
-      break
-    case '/scripts.js':
-      sendFile(res, 'scripts.js', 'text/javascript')
-      break
-    case '/server.js':
-      sendFile(res, 'server.js', 'text/javascript')
-      break
-    case '/readme.md':
-    case '/README.md':
-      sendReadme(res)
-      break
-    default:
-      res.end('404 not found')
-  }
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount), 
+  databaseURL: "https://helpme-151520.firebaseio.com/"
 })
 
-server.listen(process.env.PORT || port)
-console.log('listening on 8080')
+var db = admin.database()
+var ref = db.ref("/") //use ref to get database objects
 
-function sendReadme(res) {
-  var contentType = 'text/html'
-  fs.readFile(__dirname + '/README.md', 'utf8', function(err, md) {
-    if (err) {
-      throw err
-    }
-    
-    //Serve rendered readme
-    res.writeHead(200, {'Content-type': contentType})
-    res.end(marked(md), 'utf-8')
-  })
-}
-
-function sendFile(res, filename, contentType) {
-  contentType = contentType || 'text/html'
-
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': contentType})
-    res.end(content, 'utf-8')
-  })
-}
+// https://firebase.google.com/docs/database/admin/retrieve-data
+// https://firebase.google.com/docs/database/admin/save-data
+ref = db.ref("/profiles")
+ref.on("child_added", function(snapshot, prevChildKey) {
+  var newPost = snapshot.val()
+  console.log(newPost)
+})
