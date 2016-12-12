94613c89dbcb409a16182c20a08df52ca6f86813
Arthur Lockman <hello@rthr.me>
2016-12-11 23:37:14 -0500

    Added notification engine

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http   = require('http')
   , fs     = require('fs')
   , url    = require('url')
   , admin  = require('firebase-admin')
-  , fcmapi = require('fcm-push')
+  , fcmApi    = require('fcm-node')
 
 //Configure firebase connection
 var serviceAccount = require("./firebase.json")
@@ -10,7 +10,7 @@ var cloudMessagingAccount = require("./cloudmessaging.json")
 
 //Configure firebase push
 //https://hasura.io/blog/quick-howto-fcm-push-notifications-with-nodejs/
-var fcm = fcmapi(cloudMessagingAccount.server_key)
+var fcm = new fcmApi(cloudMessagingAccount.server_key)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount), 
@@ -19,17 +19,57 @@ admin.initializeApp({
 
 var db = admin.database()
 var ref = db.ref("/") //use ref to get database objects
+var profilesRef = db.ref("/profiles")
+var inboxRef = db.ref("/request-inbox")
 
 // https://firebase.google.com/docs/database/admin/retrieve-data
 // https://firebase.google.com/docs/database/admin/save-data
-ref = db.ref("/profiles")
-ref.on("child_added", function(snapshot, prevChildKey) {
-  var newPost = snapshot.val()
-  console.log(newPost)
-})
 
-var inboxRef = db.ref("/request-inbox")
 inboxRef.on("child_added", function(snapshot, prevChildKey) {
   var newHelpRequest = snapshot.val()
   console.log("New help request: " + newHelpRequest)
+  
 })
+
+function sendHelpRequestNotifications(keywords, requestTitle, requestBody)
+{
+  profilesRef.once('value', function(v) {
+    // console.log(v.val())
+    for (var attributename in v.val()) {
+      var userKeywords = v.val()[attributename]['filters']
+      var token = v.val()[attributename]['deviceToken']
+      var email = v.val()[attributename]['email']
+      var matches = keywords.some(function (v) {
+        return userKeywords.indexOf(v) >= 0
+      })
+      if (matches) {
+        console.log('Dispatching notification to user ' + email + ' with token ' + token)
+        sendNotificaton(token, requestTitle, requestBody)
+      }
+    }
+  })
+}
+
+function sendNotificaton(token, title, body) {
+  var message = {
+          to: token,
+          data: {
+            request_id: 'undefined'
+          },
+          priority: 'high',
+          notification: {
+            title: title,
+            body: body
+          }
+        }
+  fcm.send(message, function(err, response) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Notification sent, response: " + response)
+    }
+  })
+}
+
+sendHelpRequestNotifications(['java', 'music'], 'Test Request', 'I need help with math and Java!')
+sendHelpRequestNotifications(['test_value_1'], 'This is a test', 'Test request from the server')
